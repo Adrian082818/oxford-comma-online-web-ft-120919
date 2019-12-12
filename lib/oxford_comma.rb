20e0838913c9b1fd8727e3ca1c17f33446de27b1
Adrian Mohabir <mohabiradrian@gmail.com>
2019-12-12 15:04:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
 if array.length == 1 
   return array[0] 
-  elsif array.length == 2 
-  return array.join(",")
+ elsif array.length > 1 && array.length < 3
+        array.join(" and ")
   else array.length > 2
         array[-1].insert(0, "and ")
         array.join(", ")
